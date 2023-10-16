b7c9e9b36a8e6a771efa5b2254d07eb06ae00e86
Victor Gonzalez Morales <victorgm2709@gmail.com>
2023-10-16 14:45:02 +0200

    some fixes

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from game import Game, end_game
 
 if __name__ == '__main__':
 
+  ''' Return True if the player wants to start the game '''
   if Menu():
 
     snake_game = Game()
     start = True
     while start:
       
-      if snake_game.start():
+      if snake_game.start(): # if return False, player wants to end the game
         start = False
       
-      elif snake_game.game_over():
+      elif snake_game.game_over(): # if return False, player wants to end the game
         start = False
         
   end_game()
